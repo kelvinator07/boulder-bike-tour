1680aa863b4394625d3354a9a170fa2d30de2e2a
isievwore <isievwore@systemspecs.com.ng>
2020-08-25 00:13:05 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Layout from './components/Layout/Layout';
+import { Route, Switch, Redirect } from "react-router-dom";
+import routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div>
+            <Layout>
+              {/* <Route path="/" exact component={Home}/> */}
+              <Redirect to="/home" />
+              <Switch>
+                  {routes.map((prop, key) => {
+                  return (
+                      <Route
+                      path={prop.path}
+                      component={prop.component}
+                      key={key}
+                      />
+                  );
+                  })}
+              </Switch>
+            </Layout>
+        </div>
   );
 }
 
